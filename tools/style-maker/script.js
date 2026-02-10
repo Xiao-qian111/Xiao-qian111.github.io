b9433dccc62aca8d3e68ed4858804dab8e47dab4
SCXG <144230175+Xiao-qian111@users.noreply.github.com>
2026-02-10 22:15:25 +0800

    Create script.js

---
+++ tools/style-maker/script.js
@@ -0,0 +1,24 @@
+function changeStyle(var id, var sty, var value) {
+    document.getElementById(id).style[sty] = value;
+}
+
+function check(var id) {
+    return document.getElementById(id).checked;
+}
+
+function preview() {
+    if(check("Bold")) {
+        changeStyle("previewt", "font-weight", "bold");
+    }
+    if(check("Italics")) {
+        changeStyle("previewt", "font-style", "italic");
+    }
+    if(check("Underline")) {
+        changeStyle("previewt", "text-decoration", "underline");
+    }
+    /*
+    if(check("Strikethrough")) {
+        changeStyle("previewt", "font-style", "italic");
+    }
+    */
+}
