9dcdcce804fdc61434d944cb538910bffc100cd9
SCXG <144230175+Xiao-qian111@users.noreply.github.com>
2026-02-21 20:28:18 +0800

    Update script.js

--- tools/style-maker/script.js
+++ tools/style-maker/script.js
@@ -6,6 +6,62 @@ function changeStyle(id, sty, value) {
 function check(id) {
     return document.getElementById(id).checked;
 }
+/* Change color */
+function colorType() {
+    const select = document.getElementById('c-type');
+    const container = document.getElementById('color-controls-container');
+    const val = select.value;
+    switch(val) {
+        /* RGB color */
+        case "rgbc":
+            container.innerHTML = '
+                R: <input type="number" id="rgb-r" min="0" max="255" value="0">
+                G: <input type="number" id="rgb-g" min="0" max="255" value="0">
+                B: <input type="number" id="rgb-b" min="0" max="255" value="0">
+            ';
+            break;
+        /* HEX color */
+        case "hexc":
+            container.innerHTML = '
+                HEX: <input type="text" id="hex-code" value="#000000" placeholder="#XXXXXX">
+            ';
+            break;
+        /* Color picker */
+        case "pick":
+            container.innerHTML = '
+                Color: <input type="color" id="color-picker" value="#000000">
+            ';
+            break;
+        /* Color picker */
+        case "html":
+            container.innerHTML = '
+                <select id="c-type">
+                    <option value="Red">Red</option>
+                    <option value="Orange">Orange</option>
+                    <option value="Gold">Gold</option>
+                    <option value="Yellow">Yellow</option>
+                    <option value="Lime">Lime</option>
+                    <option value="GreenYellow">Green Yellow</option>
+                    <option value="Green">Green</option>
+                    <option value="Olive">Olive</option>
+                    <option value="Cyan">Cyan</option>
+                    <option value="Turquoise">Turquoise</option>
+                    <option value="Teal">Teal</option>
+                    <option value="Blue">Blue</option>
+                    <option value="Purple">Purple</option>
+                    <option value="Pink">Pink</option>
+                    <option value="Brown">Brown</option>
+                    <option value="Tan">Tan</option>
+                    <option value="White">White</option>
+                    <option value="WhiteSmoke">White Smoke</option>
+                    <option value="Silver">Silver</option>
+                    <option value="Gray">Gray</option>
+                    <option value="Black">Black</option>
+                </select>
+            ';
+            break;
+    }
+}
 /* Function of preview */
 function preview() {
     /* Reset style */
@@ -13,20 +69,16 @@ function preview() {
     changeStyle("previewt", "font-style", "normal");
     changeStyle("previewt", "text-decoration", "none");
     /* Change style */
-    if(check("Bold")) {
-        changeStyle("previewt", "font-weight", "bold");
+    if(check("Bold")) changeStyle("previewt", "font-weight", "bold");
+    if(check("Italics")) changeStyle("previewt", "font-style", "italic");
+    /* Lines */
+    let lines = [];
+    if(check("Overline")) lines.push("overline");
+    if(check("Underline")) lines.push("underline");
+    if(check("Strikethrough")) lines.push("line-through");
+    if(lines.length > 0) {
+        changeStyle("previewt", "text-decoration", lines.join(" "));
     }
-    if(check("Italics")) {
-        changeStyle("previewt", "font-style", "italic");
-    }
-    if(check("Underline")) {
-        changeStyle("previewt", "text-decoration", "underline");
-    }
-    /*
-    if(check("Strikethrough")) {
-        changeStyle("previewt", "font-style", "italic");
-    }
-    */
 }
 /* This function is just for test */
 function test() {
