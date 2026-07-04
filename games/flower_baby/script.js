6b2cae94a5e11a3a823948cbd81105b497b8c698
SCXG <144230175+Xiao-qian111@users.noreply.github.com>
2026-07-04 17:28:02 +0800

    Fix language setting in setLang function

--- games/flower_baby/script.js
+++ games/flower_baby/script.js
@@ -11,9 +11,10 @@ var trans = {
 }
 
 function setLang(lang) {
-    var keys = Objects.keys(trans);
+    var temp = trans[lang];
+    var keys = Objects.keys(temp);
     for(var i = 0; i < keys.length; i++) {
-        document.getElementById(keys[i]).innerHTML = trans[lang][keys[i]];
+        document.getElementById(keys[i]).innerHTML = temp[keys[i]];
     }
 }
 setLang("en");
