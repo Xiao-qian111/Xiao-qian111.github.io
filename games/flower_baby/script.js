67f949921f0d47b1db744b5aeb5ebc09ff28adda
SCXG <144230175+Xiao-qian111@users.noreply.github.com>
2026-07-04 17:07:13 +0800

    Create script.js

---
+++ games/flower_baby/script.js
@@ -0,0 +1,18 @@
+var trans = {
+    "en": {
+        "flow": "This is your <s>ugly</s> flower baby:"
+    }, 
+    "zh-hans": {
+        "flow": "这是你的<s>丑恶的</s>花宝宝："
+    }, 
+    "hyw": {
+        "flow": "这是汝的<s>58</s>花**："
+    }
+}
+
+function setLang(lang) {
+    var keys = Objects.keys(trans);
+    for(var i = 0; i < keys.length; i++) {
+        document.getElementById(keys[i]).innerHTML = trans[lang][keys[i]];
+    }
+}
